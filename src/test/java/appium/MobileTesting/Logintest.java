939215eb2892c2c4a9191b4af704c357cf6a72b3
meow <reshmakumar489@gmail.com>
2024-01-09 11:27:56 +0530

    test file fix

--- src/test/java/appium/MobileTesting/Logintest.java
+++ src/test/java/appium/MobileTesting/Logintest.java
@@ -11,21 +11,12 @@ public class Logintest{
   public static void testAppium1() throws IOException, InterruptedException {
 	  
 	  //launching emulator and waiting for boot completion
-<<<<<<< HEAD
 	  //AppiumSetup.launchEmulator();
 	  //AppiumSetup.waitForEmulator();
 	  //AppiumSetup.setEmulatorTimezone();
 	  
 	  //starting the appium server and verifying if it is listening in the required IP and port
 	  //AppiumSetup.server();
-=======
-	  AppiumSetup.launchEmulator();
-	  AppiumSetup.waitForEmulator();
-	  //AppiumSetup.setEmulatorTimezone();
-	  
-	  //starting the appium server and verifying if it is listening in the required IP and port
-	  AppiumSetup.server();
->>>>>>> 58e2820faf85d89dce13357acac1d3cb005f3f46
 	  
 	  //initiating Android driver instance
 	  AndroidDriver driver = AppiumSetup.initiateDriver();
@@ -34,13 +25,5 @@ public class Logintest{
 	  //login
 	  MobileActions.login(driver);
   }
-<<<<<<< HEAD
-=======
-  
-  @Test
-  public void testAppium2() {
-	  
-  }
->>>>>>> 58e2820faf85d89dce13357acac1d3cb005f3f46
 }
 
