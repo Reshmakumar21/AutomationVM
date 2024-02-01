cf9346412a47cf899426d1343d36091471763fe8
Reshmakumar21 <72339723+Reshmakumar21@users.noreply.github.com>
2024-02-01 16:10:50 +0530

    Update MobileUI.java

--- src/test/java/appium/MobileTesting/MobileUI.java
+++ src/test/java/appium/MobileTesting/MobileUI.java
@@ -8,7 +8,8 @@ import io.appium.java_client.android.AndroidDriver;
 public class MobileUI {
 	
 	public static WebElement btn_popupclose(AndroidDriver driver) {
-
+		
+		System.out.println("About to click on close");
 		return MobileActions.getWhenVisible(By.id("cx.indianoil.in:id/button_close"), driver);
 	}
 
@@ -19,21 +20,21 @@ public class MobileUI {
 	}
 	
 	
-	public static WebElement btn_menu(AndroidDriver driver) {
+	public static WebElement btn_newconnection(AndroidDriver driver) {
 
-		System.out.println("Clicking on menu");
+		System.out.println("Clicking on Order Cylinder");
 		return MobileActions.getWhenVisible(By.id("cx.indianoil.in:id/text_card_cylinder"),driver);
 	}
 	
 	public static WebElement btn_login(AndroidDriver driver) {
 
-		System.out.println("Clicking on sign in");
+		System.out.println("Clicking on login button");
 		return MobileActions.getWhenVisible(By.id("cx.indianoil.in:id/tv_bottom_dialog_login"),driver);
 	}
 
 	public static WebElement btn_username(AndroidDriver driver) {
 
-		System.out.println("entering user name");
+		System.out.println("Entering user name");
 		return MobileActions.getWhenVisible(By.xpath("//android.widget.EditText[@resource-id='cx.indianoil.in:id/email']"),driver);
 	}
 
